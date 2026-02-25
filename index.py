7a6b443f0ac0f01bd6ebeca6eb0e86f85570bce6
SamyBO98 <90256223+SamyBO98@users.noreply.github.com>
2026-02-25 12:25:45 +0100

    Add case where user enters something not in the list

--- index.py
+++ index.py
@@ -3,7 +3,13 @@ from chatterbot.trainers import ListTrainer
 
 #read_only False = bot can learn from the conversation
 #chatterbot.logic.BestMatch = compare message to sentences he knows -> get the closest answer
-bot = ChatBot("chatbot", read_only=False, logic_adapter=["chatterbot.logic.BestMatch"])
+listAdapter = {
+    "import_path":"chatterbot.logic.BestMatch",
+    "default_response":"Sorry I dont have any answer",
+}
+
+bot = ChatBot("chatbot", read_only=False, 
+              logic_adapter=[listAdapter])
 
 #Question and Answer
 list_to_train = [
@@ -12,7 +18,17 @@ list_to_train = [
     "What's your name?",
     "I am a chatbot",
     "How old are you ?",
-    "I am very old"
+    "I am very old",
+    "Why are you so mad ?",
+    "I am not",
+    "Test",
+    "Test validate",
+    "Can you help me ?",
+    "Of course how can I help you ?",
+    "What game do you like ?",
+    "I love League of Legends",
+    "Are you ok ?",
+    "Always ok"
 
 ]
 
@@ -23,4 +39,10 @@ list_trainer.train(list_to_train)
 while True:
     user_response = input("User : ")
     res = bot.get_response(user_response)
-    print("Chatbot : " +str(res))
+    if(res.confidence > 0.9):
+        print("Chatbot : " +str(res))
+    else:
+
+        print("Chatbot : " + str(listAdapter["default_response"]))
+    
+    print("Confidence : ", res.confidence)
